790284ad7ae3af49cb922b8a563d10991c182586
Ricardo M. <rmoctezumaf@gmail.com>
2025-04-29 17:24:43 -0500

    Added input on panel

--- src/RMMute.cpp
+++ src/RMMute.cpp
@@ -6,6 +6,7 @@ struct RMMute : Module {
 		PARAMS_LEN
 	};
 	enum InputId {
+		__INPUT,
 		INPUTS_LEN
 	};
 	enum OutputId {
@@ -17,6 +18,7 @@ struct RMMute : Module {
 
 	RMMute() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configInput(__INPUT, "");
 	}
 
 	void process(const ProcessArgs& args) override {
@@ -33,6 +35,8 @@ struct RMMuteWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(126.199, 88.093)), module, RMMute::__INPUT));
 	}
 };
 
